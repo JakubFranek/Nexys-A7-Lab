6cb9e76609209239a5de7d887134c4dbeb3ac11b
Jakub Franek <j.franek95@gmail.com>
2025-05-09 18:27:52 +0200

    Improve ps2_transceiver
    Add TX support to ps2_transceiver.
    Improve ps2_transceiver_tb.

--- source/ps2/ps2_transceiver/ps2_transceiver.vhd
+++ source/ps2/ps2_transceiver/ps2_transceiver.vhd
@@ -5,6 +5,7 @@ library ieee;
 
 entity ps2_transceiver is
     generic (
+        -- System clock frequency in Hz
         CLOCK_FREQUENCY : natural := 100_000_000
     );
     port (
@@ -13,9 +14,9 @@ entity ps2_transceiver is
         --! Synchronous reset signal
         i_reset : in    std_logic;
         --! PS/2 clock signal
-        io_ps2_clk : inout std_logic;
+        io_ps2_clk : inout std_logic := 'Z';
         --! PS/2 data signal
-        io_ps2_data : inout std_logic;
+        io_ps2_data : inout std_logic := 'Z';
         --! TX start transmission (active high)
         i_tx_start : in    std_logic;
         --! TX data to be sent to device
@@ -39,33 +40,43 @@ end entity ps2_transceiver;
 
 architecture rtl of ps2_transceiver is
 
-    signal clk_ena_1us : std_logic;
+    signal clk_ena_1us : std_logic; -- 1 MHz multi-purpose clock
 
-    signal ps2_clk_clean  : std_logic;
-    signal ps2_data_clean : std_logic;
+    signal reset : std_logic;
 
-    signal ps2_clk_falling : std_logic;
-    signal ps2_clk_rising  : std_logic;
+    signal ps2_clk_clean  : std_logic; -- Synchronized and debounced PS/2 clock line
+    signal ps2_data_clean : std_logic; -- Synchronized and debounced PS/2 data line
+
+    signal ps2_clk_falling : std_logic; --
 
     -- Shift register: Start bit (MSB), 8 data bits, stop bit, odd parity bit (LSB)
-    signal shiftreg         : std_logic_vector(10 downto 0) := (others => '0');
-    signal shiftreg_counter : unsigned(3 downto 0)          := (others => '0');
-    signal rx_parity        : std_logic;
+    signal q_shiftreg         : std_logic_vector(10 downto 0) := (others => '0');
+    signal q_shiftreg_counter : unsigned(3 downto 0)          := (others => '0');
+    -- Maximum value of shift register counter, 1 (start bit) + 8 (data bits) + 1 (parity) + 1 (stop bit) = 11
+    constant SHIFTREG_COUNTER_MAX : natural := 11;
 
     type t_fsm_state is (
         S_RX,
         S_INHIBIT,
         S_TX,
-        S_TX_DONE
+        S_TX_DONE,
+        S_RESET
     );
 
-    signal current_state : t_fsm_state := S_RX;
+    signal state : t_fsm_state := S_RX;
 
     constant INHIBIT_COUNTER_MAX   : natural := 100;
     constant INHIBIT_COUNTER_WIDTH : natural := get_binary_width(INHIBIT_COUNTER_MAX);
 
     signal inhibit_counter       : unsigned(INHIBIT_COUNTER_WIDTH - 1 downto 0) := (others => '0');
     signal inhibit_counter_reset : std_logic;
+
+    constant WATCHDOG_COUNTER_MAX   : natural := 2000; -- 2 ms when counted by the `clk_ena_1us` clock
+    constant WATCHDOG_COUNTER_WIDTH : natural := get_binary_width(WATCHDOG_COUNTER_MAX);
+
+    signal watchdog_reset    : std_logic;
+    signal watchdog_overflow : std_logic;
+    signal watchdog_count    : unsigned(WATCHDOG_COUNTER_WIDTH - 1 downto 0) := (others => '0');
 
 begin
 
@@ -85,7 +96,7 @@ begin
             PERIOD => 4
         )
         port map (
-            i_input   => io_ps2_clk,
+            i_input   => to_x01(io_ps2_clk),
             i_clk     => i_clk,
             i_clk_ena => clk_ena_1us,
             o_output  => ps2_clk_clean
@@ -97,7 +108,7 @@ begin
             PERIOD => 4
         )
         port map (
-            i_input   => io_ps2_data,
+            i_input   => to_x01(io_ps2_data),
             i_clk     => i_clk,
             i_clk_ena => clk_ena_1us,
             o_output  => ps2_data_clean
@@ -107,7 +118,7 @@ begin
         port map (
             i_clk          => i_clk,
             i_input        => ps2_clk_clean,
-            o_rising_edge  => ps2_clk_rising,
+            o_rising_edge  => open,
             o_falling_edge => ps2_clk_falling,
             o_any_edge     => open
         );
@@ -120,43 +131,56 @@ begin
         )
         port map (
             i_clk       => i_clk,
-            i_enable    => '1',
+            i_enable    => clk_ena_1us,
             i_direction => '1',
             i_reset     => inhibit_counter_reset,
             o_count     => inhibit_counter
+        );
+
+    inst_watchdog_counter : entity work.counter
+        generic map (
+            WIDTH       => WATCHDOG_COUNTER_WIDTH,
+            COUNTER_MAX => WATCHDOG_COUNTER_MAX
+        )
+        port map (
+            i_clk       => i_clk,
+            i_enable    => clk_ena_1us,
+            i_direction => '1',
+            i_reset     => watchdog_reset,
+            o_count     => watchdog_count,
+            o_overflow  => watchdog_overflow
         );
 
     -------------------------------------------------------------------------------------------------------------------
 
     proc_fsm : process (i_clk) is
 
-        variable v_next_state : t_fsm_state; --! Next value of `current_state`
+        variable v_next_state : t_fsm_state; --! Next value of `state`
 
     begin
 
         if rising_edge(i_clk) then
             if (i_reset = '1') then
-                current_state <= S_RX;
+                state <= S_RESET;
             else
                 -- Default assignments
-                v_next_state          := current_state;
-                inhibit_counter_reset <= '1';
-                io_ps2_clk            <= 'Z';
-                o_tx_busy             <= '0';
+                v_next_state := state;
 
-                case current_state is
+                case state is
+
+                    when S_RESET =>
+
+                        v_next_state := S_RX;
 
                     when S_RX =>
 
                         if (i_tx_start = '1') then
                             v_next_state := S_INHIBIT;
+                        elsif (watchdog_overflow = '1' and o_rx_busy = '1') then
+                            v_next_state := S_RESET;
                         end if;
 
                     when S_INHIBIT =>
-
-                        inhibit_counter_reset <= '0';
-                        io_ps2_clk            <= '0';
-                        o_tx_busy             <= '1';
 
                         if (inhibit_counter = INHIBIT_COUNTER_MAX) then
                             v_next_state := S_TX;
@@ -164,15 +188,13 @@ begin
 
                     when S_TX =>
 
-                        o_tx_busy <= '1';
-
                         if (o_tx_done = '1') then
                             v_next_state := S_TX_DONE;
+                        elsif (watchdog_overflow = '1') then
+                            v_next_state := S_RESET;
                         end if;
 
                     when S_TX_DONE =>
-
-                        o_tx_busy <= '1';
 
                         if (ps2_clk_clean = '1' and ps2_data_clean = '1') then
                             v_next_state := S_RX;
@@ -180,85 +202,140 @@ begin
 
                     when others =>
 
+                        assert false
+                            report "Invalid PS/2 transceiver state."
+                            severity failure;
+                        v_next_state := S_RX;
+
                 end case;
 
+                state <= v_next_state;
             end if;
         end if;
 
     end process proc_fsm;
 
+    -------------------------------------------------------------------------------------------------------------------
+    proc_fsm_comb : process (all) is
+    begin
+
+        -- Default assignments
+        inhibit_counter_reset <= '1';
+        io_ps2_clk            <= 'Z';
+        io_ps2_data           <= 'Z';
+        o_tx_busy             <= '0';
+        o_rx_busy             <= '0';
+        watchdog_reset        <= '1';
+
+        case state is
+
+            when S_RESET =>
+
+                reset <= '1';
+
+            when S_RX =>
+
+                o_rx_busy      <= '1' when (q_shiftreg_counter /= 0) else
+                                  '0';
+                watchdog_reset <= '0' when (q_shiftreg_counter /= 0) else
+                                  '1';
+
+            when S_INHIBIT =>
+
+                inhibit_counter_reset <= '0';
+                io_ps2_clk            <= '0';
+                o_tx_busy             <= '1';
+
+            when S_TX =>
+
+                o_tx_busy      <= '1';
+                watchdog_reset <= '0';
+                io_ps2_data    <= 'Z' when q_shiftreg(q_shiftreg'low) = '1' else '0';
+
+            when S_TX_DONE =>
+
+                o_tx_busy <= '1';
+
+            when others =>
+
+                assert false
+                    report "Invalid PS/2 transceiver state."
+                    severity failure;
+
+        end case;
+
+    end process proc_fsm_comb;
+
+    -------------------------------------------------------------------------------------------------------------------
     proc_shiftreg : process (i_clk) is
+
+        variable v_rx_parity : std_logic;
+
     begin
 
         if rising_edge(i_clk) then
-            -- Default assignments
-            io_ps2_data <= 'Z';
+            if (reset = '1' or (state = S_TX_DONE)) then
+                q_shiftreg         <= (others => '0');
+                q_shiftreg_counter <= (others => '0');
+                o_rx_data          <= (others => '0');
+                o_rx_error         <= '0';
+                o_rx_done          <= '0';
+                o_tx_done          <= '0';
+                o_tx_error         <= '0';
+            elsif (state = S_RX) then
+                -- Default assignments
+                o_rx_done <= '0';
 
-            if (i_reset = '1' or (current_state /= S_RX and current_state /= S_TX)) then
-                shiftreg         <= (others => '0');
-                shiftreg_counter <= (others => '0');
-                o_rx_data        <= (others => '0');
-                o_rx_done        <= '0';
-            elsif (current_state = S_RX) then
                 if (ps2_clk_falling = '1') then
-                    shiftreg <= shiftreg(9 downto 0) & ps2_data_clean;
+                    q_shiftreg <= ps2_data_clean & q_shiftreg(10 downto 1);
 
-                    if (shiftreg_counter < 11) then
-                        shiftreg_counter <= shiftreg_counter + 1;
+                    if (q_shiftreg_counter < SHIFTREG_COUNTER_MAX) then
+                        q_shiftreg_counter <= q_shiftreg_counter + 1;
                     end if;
                 end if;
 
-                -- Default assignment
-                o_rx_done <= '0';
+                if (q_shiftreg_counter = SHIFTREG_COUNTER_MAX) then
+                    q_shiftreg_counter <= (others => '0');
+                    q_shiftreg         <= (others => '0');
 
-                if (shiftreg_counter = 11) then
-                    shiftreg_counter <= (others => '0');
+                    v_rx_parity := xor q_shiftreg(9 downto 1);
 
-                    if (o_rx_error = '0') then
-                        o_rx_data <= shiftreg(9 downto 2);
+                    -- Check parity, start bit and stop bit
+                    if (v_rx_parity /= '1' or q_shiftreg(q_shiftreg'high) /= '1'
+                        or q_shiftreg(q_shiftreg'low) /= '0') then
+                        o_rx_error <= '1';
+                    else
+                        o_rx_data <= q_shiftreg(8 downto 1);
                         o_rx_done <= '1';
                     end if;
                 end if;
-            elsif (current_state = S_TX) then
-                if (shiftreg_counter = 0) then
-                    -- Start bit
-                    shiftreg(10) <= '0';
-                    -- Data
-                    shiftreg(9 downto 2) <= i_tx_data;
-                    -- Parity bit
-                    shiftreg(1) <= xor shiftreg(9 downto 1);
-                -- Stop bit is not included in shift register (transmitted by releasing the data line to high)
-                end if;
-
+            elsif (state = S_INHIBIT) then
+                -- Load shift register with data to be sent
+                -- Start bit
+                q_shiftreg(0) <= '0';
+                -- TX data
+                q_shiftreg(8 downto 1) <= i_tx_data;
+                -- Parity bit
+                q_shiftreg(9) <= not(xor i_tx_data);
+                -- Stop bit
+                q_shiftreg(10) <= '1';
+            elsif (state = S_TX) then
                 if (ps2_clk_falling = '1') then
-                    shiftreg <= shiftreg(9 downto 0) & '0';
+                    -- Loading the shiftreg with '1's ensures that the line will be naturally high-Z after transmit
+                    q_shiftreg <= '1' & q_shiftreg(10 downto 1);
 
-                    if (shiftreg_counter < 10) then
-                        shiftreg_counter <= shiftreg_counter + 1;
-
-                        io_ps2_data <= shiftreg(10);
-                        if (shiftreg_counter = 9) then
-                            io_ps2_data <= 'Z';
-                        end if;
-                    end if;
-                elsif (ps2_clk_rising = '1') then
-                    if (shiftreg_counter = 10) then
-                        o_tx_done        <= '1';
-                        shiftreg_counter <= (others => '0');
-                        o_tx_error       <= '1' when io_ps2_data /= '0' else '0';
+                    if (q_shiftreg_counter < SHIFTREG_COUNTER_MAX) then
+                        q_shiftreg_counter <= q_shiftreg_counter + 1;
+                    elsif (q_shiftreg_counter = SHIFTREG_COUNTER_MAX) then
+                        o_tx_done          <= '1';
+                        o_tx_error         <= '1' when io_ps2_data /= '0' else '0';
+                        q_shiftreg_counter <= (others => '0');
                     end if;
                 end if;
             end if;
         end if;
 
     end process proc_shiftreg;
-
-    rx_parity  <= xor shiftreg(9 downto 1);
-    o_rx_busy  <= '1' when (current_state = S_RX and shiftreg_counter /= 0 and shiftreg_counter /= 11) else
-                  '0';
-    o_rx_error <= '1' when (current_state = S_RX and shiftreg_counter = 11 and
-                               (rx_parity /= '1' or shiftreg(10) /= '0' or shiftreg(0) /= '1')) else
-                  '0';
 
 -------------------------------------------------------------------------------------------------------------------
 
--- testbench/ps2/ps2_transceiver/ps2_transceiver_tb.vhd
+++ testbench/ps2/ps2_transceiver/ps2_transceiver_tb.vhd
@@ -13,11 +13,12 @@ end entity ps2_transceiver_tb;
 architecture tb of ps2_transceiver_tb is
 
     signal   clk            : std_logic := '1';
-    signal   ps2_clk        : std_logic := '1';
-    signal   ps2_data       : std_logic := '1';
+    signal   ps2_clk        : std_logic := 'H';
+    signal   ps2_data       : std_logic := 'H';
     constant CLK_PERIOD     : time      := 10 ns;
     constant PS2_CLK_PERIOD : time      := 60 us;
 
+    signal reset    : std_logic                    := '0';
     signal tx_start : std_logic                    := '0';
     signal tx_data  : std_logic_vector(7 downto 0) := (others => '0');
     signal rx_data  : std_logic_vector(7 downto 0);
@@ -27,6 +28,8 @@ architecture tb of ps2_transceiver_tb is
     signal rx_busy  : std_logic;
     signal rx_done  : std_logic;
     signal rx_error : std_logic;
+
+    signal tb_shiftreg : std_logic_vector(9 downto 0) := (others => '0');
 
     procedure send_ps2_data (
         signal clock : out std_logic;
@@ -42,42 +45,74 @@ architecture tb of ps2_transceiver_tb is
         wait for 0.5 * PS2_CLK_PERIOD;
         clock <= '0';
         wait for 0.5 * PS2_CLK_PERIOD;
-        clock <= '1';
+        clock <= 'Z';
 
         for i in 0 to 7 loop
 
-            data   <= v_data(8);
+            data   <= 'Z' when v_data(0) = '1' else '0';
             wait for 0.5 * PS2_CLK_PERIOD;
-            v_data := v_data(7 downto 0) & '0';
+            v_data := '0' & v_data(8 downto 1);
             clock  <= '0';
             wait for 0.5 * PS2_CLK_PERIOD;
-            clock  <= '1';
+            clock  <= 'Z';
 
         end loop;
 
-        data  <= xor v_data; -- Odd parity bit
+        data  <= 'Z' when (xor to_x01(v_data) = '1') else '0'; -- Odd parity bit
         wait for 0.5 * PS2_CLK_PERIOD;
         clock <= '0';
         wait for 0.5 * PS2_CLK_PERIOD;
-        clock <= '1';
-        data  <= '1';        -- Stop bit
+        clock <= 'Z';
+        data  <= 'Z';                                          -- Stop bit
         wait for 0.5 * PS2_CLK_PERIOD;
         clock <= '0';
         wait for 0.5 * PS2_CLK_PERIOD;
-        clock <= '1';
+        clock <= 'Z';
 
     end procedure send_ps2_data;
+
+    procedure receive_ps2_data (
+        signal clock    : out std_logic;
+        signal data     : inout std_logic;
+        signal shiftreg : inout std_logic_vector(9 downto 0)
+    ) is
+    begin
+
+        wait for 0.5 * PS2_CLK_PERIOD;
+        shiftreg <= (others => '0');
+
+        for i in 0 to 9 loop
+
+            clock    <= '0';
+            wait for 0.5 * PS2_CLK_PERIOD;
+            clock    <= 'Z';
+            shiftreg <= to_x01(data) & tb_shiftreg(9 downto 1);
+            wait for 0.5 * PS2_CLK_PERIOD;
+
+        end loop;
+
+        clock <= '0';
+        wait for 0.5 * PS2_CLK_PERIOD;
+        clock <= 'Z';
+        data  <= '0'; -- Acknowledge bit
+        wait for 0.5 * PS2_CLK_PERIOD;
+        clock <= '0';
+        wait for 0.5 * PS2_CLK_PERIOD;
+        clock <= 'Z';
+        data  <= 'Z';
+
+    end procedure receive_ps2_data;
 
 begin
 
     inst_uut : entity work.ps2_transceiver
         port map (
             i_clk       => clk,
-            i_reset     => '0',
+            i_reset     => reset,
             io_ps2_clk  => ps2_clk,
             io_ps2_data => ps2_data,
-            i_tx_start  => '0',
-            i_tx_data   => (others => '0'),
+            i_tx_start  => tx_start,
+            i_tx_data   => tx_data,
             o_tx_busy   => tx_busy,
             o_tx_done   => tx_done,
             o_tx_error  => tx_error,
@@ -89,26 +124,52 @@ begin
 
     clk <= not clk after (CLK_PERIOD / 2);
 
+    -- Pull-ups
+    ps2_clk  <= 'H';
+    ps2_data <= 'H';
+
     proc_test_runner : process is
+
+        variable v_timestamp : time := 0 ns;
+
     begin
 
         test_runner_setup(runner, RUNNER_CFG);
 
         show(get_logger(default_checker), display_handler, pass);
 
-        /*wait for PS2_CLK_PERIOD;
-        send_ps2_data(ps2_clk, ps2_data, "100101101");
-        wait for PS2_CLK_PERIOD;*/
-
         while test_suite loop
 
             if run("receive") then
                 wait for PS2_CLK_PERIOD;
-                send_ps2_data(ps2_clk, ps2_data, "100101101");
+                send_ps2_data(ps2_clk, ps2_data, "110010110");
 
-                check(rx_data = "10010110", "Check the received data");
-                check(rx_error = '0', "Check there is no RX error");
-                check(rx_busy = '0', "Check RX is not busy");
+                check_equal(rx_data, std_logic_vector'("10010110"), "Check the received data");
+                check_equal(rx_error, '0', "Check there is no RX error");
+                check_equal(rx_busy, '0', "Check RX is not busy");
+
+                wait for PS2_CLK_PERIOD;
+            elsif run("transmit") then
+                tx_data     <= "10010110";
+                wait for CLK_PERIOD;
+                tx_start    <= '1';
+                wait until falling_edge(ps2_clk);
+                tx_start    <= '0';
+                v_timestamp := now;
+                wait until rising_edge(ps2_clk);
+                check_relation(now - v_timestamp >= 100 us,
+                               "Check that PS/2 clock has been held low for at least 100 us");
+                wait for CLK_PERIOD;
+                check_equal(ps2_data, '0', "Check that PS/2 data is low (TX start bit)");
+                receive_ps2_data(ps2_clk, ps2_data, tb_shiftreg);
+
+                check_equal(tb_shiftreg, std_logic_vector'("1110010110"), "Check the transmitted data");
+                check_equal(tx_error, '0', "Check there is no TX error");
+
+                wait for 5 us;
+                check_equal(tx_busy, '0', "Check TX is not busy after complete transmission");
+
+                wait for PS2_CLK_PERIOD;
             end if;
 
         end loop;
